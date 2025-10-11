6905e84ef98c93c62052735c4a5e9437e84038e9
Ira-rupich <rupich.ira@gmail.com>
2025-10-11 03:54:34 +0000

    rupich

--- city-app/src/components/CityCard/CityCard.jsx
+++ city-app/src/components/CityCard/CityCard.jsx
@@ -1,17 +1,34 @@
 const CityCard = (weather) => {
 
-    const tempCelsius = weather.main?.temp
-    const tempFahrenheit = (tempCelsius * 9/5) + 32
-    const feelsLike = weather.main?.feels_like 
-
     const getWeatherIcon = () => {
         if (weather.weather && weather.weather[0]) {
             return `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`
-        } 
+        }
     }
     return (
-       <>
-       </>
+        <div className="city-card">
+            <div className="container">
+                <div className="city-info">
+                    <img src={getWeatherIcon}
+                        alt={weather.weather[0].description} />
+                    <div className="city-meta">
+                        <span className="city-description">{weather.weather[0].description}</span>
+                        <div className="city-details">
+                            <span className="city-temp">Температура: {weather.main.temp} С°</span>
+                            <div>
+                                <span className="city-feels_like">Ощущается как: {weather.main.feels_like} С°</span>
+                            </div>
+                        </div>
+                        <div>
+                            <span className="city-pressure">Давление: {weather.main.pressure} Па</span>
+                        </div>
+                        <div>
+                            <span className="city-humidity">Влажность: {weather.main.humidity} г/м³</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 export default CityCard

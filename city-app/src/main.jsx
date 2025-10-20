61637c00c5c3e35406b85869fe6007f3c633561a
Ira-rupich <rupich.ira@gmail.com>
2025-10-20 06:55:24 +0000

    rupich

--- city-app/src/main.jsx
+++ city-app/src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import router from './router/router'
+import './index.css'
+import { TelegramProvider } from './context/TelegramContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+     <TelegramProvider>
+      <RouterProvider router={router} />
+    </TelegramProvider>
   </StrictMode>,
 )
